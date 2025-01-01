30181e0dbde841feff95c7dcb344b7f9d53de02e
theogyousef <youssef2101042@miuegypt.edu.eg>
2025-01-02 00:25:42 +0200

    fixes in profile activity and first welcome page 1/2

--- app/src/main/java/com/example/qchat/MainActivity.kt
+++ app/src/main/java/com/example/qchat/MainActivity.kt
@@ -11,6 +11,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.chatlist)
+        setContentView(R.layout.activity_profile)
     }
 }
