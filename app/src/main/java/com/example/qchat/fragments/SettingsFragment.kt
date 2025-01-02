81bba5a74e7bba42177d14fa8cc81fd3c3b65407
moazzmuhammed <moazzmuhammed123@gmail.com>
2025-01-02 17:37:06 +0200

    .

---
+++ app/src/main/java/com/example/qchat/fragments/SettingsFragment.kt
@@ -0,0 +1,80 @@
+package fragments
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.settingsuipage.R
+import com.example.settingsuipage.adapter.SettingsAdapter
+import com.example.settingsuipage.model.SettingItem
+import com.google.android.material.appbar.MaterialToolbar
+
+class SettingsFragment : Fragment() {
+
+    private val settingsItems = listOf(
+        SettingItem(
+            R.drawable.ic_key,
+            "Account",
+            "Privacy, security, change number"
+        ),
+        SettingItem(
+            R.drawable.ic_chat,
+            "Chat",
+            "Chat history, themes, wallpapers"
+        ),
+        SettingItem(
+            R.drawable.ic_notifications,
+            "Notifications",
+            "Messages, group and others"
+        ),
+        SettingItem(
+            R.drawable.ic_help,
+            "Help",
+            "Help center, contact us, privacy policy"
+        ),
+        SettingItem(
+            R.drawable.ic_storage,
+            "Storage and data",
+            "Network usage, storage usage"
+        ),
+        SettingItem(
+            R.drawable.ic_invite,
+            "Invite a friend",
+            "Share this app with friends"
+        )
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupToolbar(view)
+        setupRecyclerView(view)
+    }
+
+    private fun setupRecyclerView(view: View) {
+        val recyclerView = view.findViewById<RecyclerView>(R.id.settingsRecyclerView)
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = SettingsAdapter(settingsItems)
+        }
+    }
+
+    private fun setupToolbar(view: View) {
+        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
+        (requireActivity() as AppCompatActivity).apply {
+            setSupportActionBar(toolbar)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.title = "Settings"
+        }
+    }
+}
